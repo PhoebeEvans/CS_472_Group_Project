f8bd346a13c0cf510be32aef7df124fc7541aa31
Vince <vgnodle@gmail.com>
2024-05-04 10:22:40 -0600

    Updated Guest class

--- src/main/java/Guest.java
+++ src/main/java/Guest.java
@@ -15,7 +15,8 @@ public class Guest extends User{
     int extraGuests;
     //Reservation[] bookingHistory; //Can uncomment after reservation class is added
 
-    /**
+
+	/**
      * Constructor with parameters
      * @param firstName
      * @param lastName
@@ -52,6 +53,77 @@ public class Guest extends User{
         this.guestID = guestID;
     }
     
-    //need to add remaining getters and setters
+    
+    /**
+     * Retrieves guest's credit card number
+     * @return	creditCardNumber
+     */
+    public int getCreditCardNumber() {
+		return creditCardNumber;
+	}
+
+    
+    /**
+     * Sets guest's credit card number
+     * @param creditCardNumber
+     */
+	public void setCreditCardNumber(int creditCardNumber) {
+		this.creditCardNumber = creditCardNumber;
+	}
+
+	
+	/**
+	 * Retrieves guest's credit card expiration date
+	 * @return	creditCardExp
+	 */
+	public String getCreditCardExp() {
+		return creditCardExp;
+	}
+
+	
+	/**
+	 * Sets guest's credit card expiration date
+	 * @param creditCardExp
+	 */
+	public void setCreditCardExp(String creditCardExp) {
+		this.creditCardExp = creditCardExp;
+	}
+
+	
+	/**
+	 * Retrieves guest's credit card security code
+	 * @return	creditCardCode
+	 */
+	public int getCreditCardCode() {
+		return creditCardCode;
+	}
+
+
+	/**
+	 * Sets guest's credit card security code
+	 * @param creditCardCode
+	 */
+	public void setCreditCardCode(int creditCardCode) {
+		this.creditCardCode = creditCardCode;
+	}
+
+
+	/**
+	 * Retrieves guest's number of extra guests
+	 * @return	extraGuests
+	 */
+	public int getExtraGuests() {
+		return extraGuests;
+	}
+
+
+	/**
+	 * Sets guest's number of extra guests
+	 * @param extraGuests
+	 */
+	public void setExtraGuests(int extraGuests) {
+		this.extraGuests = extraGuests;
+	}
+
     
 }
